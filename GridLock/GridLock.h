6f2f39556fc257f26e673e5f46533f36a1330977
Matthew Wilson <mattxwilson@deloitte.co.uk>
2019-03-15 15:47:48 +0000

    initial commit

---
+++ GridLock/GridLock.h
@@ -0,0 +1,19 @@
+//
+//  GridLock.h
+//  GridLock
+//
+//  Created by Matt Wilson on 15/03/2019.
+//  Copyright © 2019 Instil. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GridLock.
+FOUNDATION_EXPORT double GridLockVersionNumber;
+
+//! Project version string for GridLock.
+FOUNDATION_EXPORT const unsigned char GridLockVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GridLock/PublicHeader.h>
+
+
---
+++ GridLockTests/GridLockTests.swift
@@ -0,0 +1,34 @@
+//
+//  GridLockTests.swift
+//  GridLockTests
+//
+//  Created by Matt Wilson on 15/03/2019.
+//  Copyright © 2019 Instil. All rights reserved.
+//
+
+import XCTest
+@testable import GridLock
+
+class GridLockTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
